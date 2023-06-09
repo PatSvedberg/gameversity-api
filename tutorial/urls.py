bba1ab5fb0cc7d307e4dac210360167ce7ba54cd
PatSvedberg <pat.svedberg@gmail.com>
2023-06-09 08:58:23 +0000

    Add update and delete tutorials

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,4 +3,5 @@ from tutorial import views
 
 urlpatterns = [
     path('tutorials/', views.TutorialList.as_view()),
+    path('tutorials/<int:pk>', views.TutorialDetail.as_view()),
 ]
--- tutorial/views.py
+++ tutorial/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Tutorial
 from .serializers import TutorialSerializer
+from gameversity_api.permissions import IsOwnerOrReadOnly
 
 
 class TutorialList(APIView):
@@ -30,3 +32,42 @@ class TutorialList(APIView):
           return Response(
               serializer.errors, status=status.HTTP_400_BAD_REQUEST
           )
+
+
+class TutorialDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = TutorialSerializer
+
+    def get_object(self, pk):
+        try:
+            tutorial = Tutorial.objects.get(pk=pk)
+            self.check_object_permissions(self.request, tutorial)
+            return tutorial
+        except Tutorial.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        tutorial = self.get_object(pk)
+        serializer = TutorialSerializer(
+            tutorial, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        tutorial = self.get_object(pk)
+        serializer = TutorialSerializer(
+            tutorial, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        tutorial = self.get_object(pk)
+        tutorial.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
