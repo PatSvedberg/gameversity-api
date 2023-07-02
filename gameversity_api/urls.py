7059a9cbc8174b7182a35e063285a7accda760ba
PatSvedberg <pat.svedberg@gmail.com>
2023-07-02 19:20:25 +0000

    Fix forgot to import logout_route

--- gameversity_api/urls.py
+++ gameversity_api/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
+
 
 urlpatterns = [
     path('', root_route),
