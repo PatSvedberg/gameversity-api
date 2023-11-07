5f8fdd295679abc0188ae462b4a8ff56d60ee5f0
PatSvedberg <pat.svedberg@gmail.com>
2023-11-07 11:01:41 +0000

    Fixed Debug

--- gameversity_api/settings.py
+++ gameversity_api/settings.py
@@ -61,7 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-patsvedberg-gameversity-8xuf98m0z7h.ws-eu100.gitpod.io', 'localhost', 'gameversity-api.herokuapp.com']
 
